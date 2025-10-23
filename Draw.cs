89172c00f48cb9744e9d36582dc599d89dd02965
dzelilaHeco <dzelila.heco@gmail.com>
2025-10-23 11:00:16 +0200

    la till person

---
+++ Draw.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tjuvochpolisfinal
+{
+    internal class Draw
+    {
+        // Rita upp staden
+        static void DrawBorder(int width, int height)
+        {
+            Console.Clear();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (y == 0 || y == height - 1 || x == 0 || x == width - 1)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.SetCursorPosition(2, 0);
+            Console.Write("City");
+
+            Console.SetCursorPosition(0, 25);
+            Console.Write("News Feed ============================");
+        }
+        static void DrawPrisonBorder(int prisonWidth, int prisonHeight, int startY, int startX)
+        {
+            for (int y = 0; y < prisonHeight; y++)
+            {
+                for (int x = 0; x < prisonWidth; x++)
+                {
+                    Console.SetCursorPosition(startX + x, y + startY);
+                    if (y == 0 || y == prisonHeight - 1 || x == 0 || x == prisonWidth - 1)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+            }
+
+            Console.SetCursorPosition(startX, startY);
+            Console.Write("Prison");
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tjuvochpolisfinal
+{
+    internal struct Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Position(int x, int y) { X = x; Y = y; }
+    }
+
+    internal struct Direction
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        private static Random rand = new Random();
+
+        public Direction(int x, int y) { X = x; Y = y; }
+
+        public static Direction RandomDirection()
+        {
+            return new Direction(rand.Next(-1, 2), rand.Next(-1, 2));
+        }
+    }
+    internal class Person
+    {
+        public string Name { get; set; }
+        public Position Position { get; set; }
+        public Direction Direction { get; set; }
+        public char Symbol { get; set; }
+        public Inventory Inventory { get; set; }
+        
+        public Person(string name, Position position, Direction direction, char symbol, Inventory inventory)
+        {
+            Name = name;
+            Position = position;
+            Direction = direction;
+            Symbol = symbol;
+            Inventory = inventory;
+        }
+        public void Move()
+        {
+            Position = new Position(Position.X + Direction.X, Position.Y + Direction.Y);
+        }
+        public void ChangeDirection()
+        {
+            Direction = Direction.RandomDirection();
+        }
+    }
+    internal class Police : Person
+    {
+        public Police(string name, Position position)
+            : base(name, position, Direction.RandomDirection(), 'P', new PoliceInventory()) { }
+    }
+
+    internal class Thief : Person
+    {
+
+        public Thief(string name, Position position)
+            : base(name, position, Direction.RandomDirection(), 'T', new ThiefInventory()) { }
+
+
+    }
+
+    internal class Citizen : Person
+    {
+        public Citizen(string name, Position position)
+            : base(name, position, Direction.RandomDirection(), 'C', new CitizenInventory()) { }
+    }
+}
